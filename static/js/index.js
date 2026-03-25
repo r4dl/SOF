83c0cfdc7e5dc9ebcd156b2702dcfd0dbf58ec38
r4dl <radl-lukas@gmx.at>
2026-03-25 20:17:02 +0100

    updated video link, removed interpolation/stacked/ reference

--- static/js/index.js
+++ static/js/index.js
@@ -83,15 +83,6 @@ $(document).ready(function () {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function (event) {
-        setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
 
 })
 
